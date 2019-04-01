c47b095c0b41c64fa9c38b900de798853e5fe7a8
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-04-01 20:14:09 +0200

    Updates

---
+++ precache-manifest.756c63b7034c9533dee844b35bbc4bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  },
+  {
+    "revision": "4f06b0cc2eec34d04bd8",
+    "url": "/ContactManager/static/js/main.ab499a29.chunk.js"
+  },
+  {
+    "revision": "a8c6911d7eef94613d40",
+    "url": "/ContactManager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "4f06b0cc2eec34d04bd8",
+    "url": "/ContactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "a8c6911d7eef94613d40",
+    "url": "/ContactManager/static/css/2.d642385c.chunk.css"
+  },
+  {
+    "revision": "69d019d85e33a98d1cb1abc39a07020e",
+    "url": "/ContactManager/index.html"
+  }
+];
